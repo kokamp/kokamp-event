708ee51187d62d20cf8ae95a65a1fbe821e24b8c
ericametepe <ericametepe@gmail.com>
2018-09-16 22:24:42 +0200

    CORS

---
+++ kokamp-event-exposition/src/main/java/com/kokamp/event/exposition/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.kokamp.event.exposition;
+
+import io.swagger.models.HttpMethod;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedHeaders("**").allowedMethods(HttpMethod.GET.name());
+
+    }
+}
